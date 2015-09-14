0f9ada0f51f0ae50b464df3a24c5c9983547f27e
Arnaud <arnaud69800@hotmail.fr>
2015-09-14 16:19:36 +0200

    Enfin finiiiiiiiiiiiiiiii!
    Signed-off-by: Arnaud <arnaud69800@hotmail.fr>

--- Exo_1/public_html/formCheck.js
+++ Exo_1/public_html/formCheck.js
@@ -23,7 +23,7 @@ var ava = document.getElementById("avancement");
 //disable le bouton
 soumettre.disabled = true;
 
-//Ajout des évènement a chque champs
+//Ajout des évènement a chaque champs
 age.addEventListener('keyup', verifAge);
 identifiant.addEventListener('keyup', verifId);
 mdp.addEventListener('keyup', verifMdp);
@@ -39,16 +39,22 @@ function verifId(){
         color(identifiant, true);
 		idIsCorrect = false;
 		enableSoumettre();
+		document.getElementById("info").innerHTML = "l'identifiant doit être compris entre 1 à 12 caractères";
+		document.getElementById("info").style.display = "block";
     } else {
         var temp = tab === null ? 0 : tab.length;
         if (temp !== identifiant.value.length) {
             color(identifiant, true);
 			idIsCorrect = false;
 			enableSoumettre();
+			document.getElementById("info").innerHTML = "l'identifiant doit être compsée que de lettre";
+			document.getElementById("info").style.display = "block";
         } else {
             color(identifiant, false);
 			idIsCorrect = true;
 			enableSoumettre();
+			document.getElementById("info").innerHTML = "";
+			document.getElementById("info").style.display = "none";
         }
     }
 }
@@ -71,14 +77,19 @@ function verifAge() {
         color(age, true);
 		ageIsCorrect = false;
 		enableSoumettre();
+		document.getElementById("info").innerHTML = "l'age minimum requis est de 18 ans";
+		document.getElementById("info").style.display = "block";
     } else {
         color(age, false);
 		ageIsCorrect = true;
         enableSoumettre();
+		document.getElementById("info").innerHTML = "";
+		document.getElementById("info").style.display = "none";
     }
 }
 
 function verifMdp() {
+	var messageAge = 'Le mots de passe doit contenir : <ul>';
     var regexp = /[A-Z]/g;
     var tab = mdp.value.match(regexp);
 
@@ -88,67 +99,110 @@ function verifMdp() {
     var regexp = /[0-9]/g;
     var tab2 = mdp.value.match(regexp);
 
-    if (mdp.value.length >= 8 && nbcar === 0)
+    if (mdp.value.length >= 8)
     {
-        avancement(20);
-        nbcar = 1;
+		if(nbcar === 0)
+		{
+			avancement(20);
+			nbcar = 1;
+		}
+		messageAge += '<li class=\"good\">Minimum 8 caractères</li>';
     }
-    else if (mdp.value.length < 8 && nbcar === 1) {
-        avancement(-20);
-        nbcar = 0;
+    else if (mdp.value.length < 8) {
+		if(nbcar === 1)
+		{
+			avancement(-20);
+			nbcar = 0;
+		}		
+		messageAge += '<li class=\"bad\">Minimum 8 caractères</li>';
     }
 
-    if (maj === 0 && tab !== null)
+    if (tab !== null)
     {
-        avancement(20);
-        maj = 1;
+		if(maj === 0)
+		{
+			avancement(20);
+			maj = 1;
+		}
+		messageAge += '<li class=\"good\">Une majuscule</li>';
     }
-    else if (tab === null && maj === 1) {
-        avancement(-20);
-        maj = 0;
+    else if (tab === null) {
+		if(maj === 1)
+		{
+			avancement(-20);
+			maj = 0;
+		}
+		messageAge += '<li class=\"bad\">Une majuscule</li>';
     }
 
-    if (min === 0 && tab1 !== null)
+    if (tab1 !== null)
     {
-        avancement(20);
-        min = 1;
+		if(min === 0)
+		{
+			avancement(20);
+			min = 1;
+		}       
+		messageAge += '<li class=\"good\">Une minuscule</li>';
     }
     else if (tab1 === null && min === 1) {
-        avancement(-20);
-        min = 0;
+		if(min === 1)
+		{
+			avancement(-20);
+			min = 0;
+		}
+		messageAge += '<li class=\"bad\">Une minuscule</li>';
     }
 
-    if (chiffre === 0 && tab2 !== null)
+    if (tab2 !== null)
     {
-        avancement(20);
-        chiffre = 1;
+		if(chiffre === 0)
+		{
+			avancement(20);
+			chiffre = 1;
+		}        
+		messageAge += '<li class=\"good\">Un chiffre</li>';
     }
-    else if (tab2 === null && chiffre === 1) {
-        avancement(-20);
-        chiffre = 0;
+    else if (tab2 === null) {
+		if(chiffre === 1)
+		{
+			avancement(-20);
+			chiffre = 0;
+		}
+		messageAge += '<li class=\"bad\">Un chiffre</li>';
     }
 
     var temp = tab !== null ? tab.length : 0;
     temp = tab1 !== null ? tab1.length + temp : temp;
     temp = tab2 !== null ? tab2.length + temp : temp;
-    if (temp !== mdp.value.length && autre === 0)
+    if (temp !== mdp.value.length)
     {
-        avancement(20);
-        autre = 1;
-    } else if (temp === mdp.value.length && autre === 1) {
+		if(autre === 0)
+		{
+			avancement(20);
+			autre = 1;
+		}
+        messageAge += '<li class="good">Un caractères spéciale</li>';
+    } else if (temp === mdp.value.length) {
+		if(autre === 1)
+		{
         avancement(-20);
         autre = 0;
+		}
+		messageAge += '<li class="bad">Un caractères spéciale</li>';
     }
-    
     if(ava.value === 100) {
         color(mdp, false);
 		mdpIsCorrect = true;
+		document.getElementById("info").style.display = "none";
     } else if(ava.value !== 100) {
         color(mdp, true);
 		mdpIsCorrect = false;
+		document.getElementById("info").innerHTML = messageAge.concat('</ul>');
+		document.getElementById("info").style.display = "block";
     }
 	verifVmdp();
 	enableSoumettre();
+	
 }
 
 function verifVmdp() {
